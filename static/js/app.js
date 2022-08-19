45515e267b1c942ff1101177551b87e011a7d7bf
Graeme Tank <graeme.tank@gmail.com>
2022-08-19 06:54:14 -0400

    handleClick function started

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,8 @@
 // import the data from data.js
 const tableData = data;
 
+// import data from "/Users/Documents/workspace/test.json"
+
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
 
@@ -25,3 +27,32 @@ function buildTable(data) {
     });
 }
 
+// 11.5.3
+// Add Filters
+
+  function handleClick() {
+    // Grab the datetime value from the filter
+    // from the original table data
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+  
+     // Check to see if a date was entered and filter the
+    // data using that date.
+    if (date) {
+      // Apply `filter` to the table data to only keep the
+      // rows where the `datetime` value matches the filter value
+      filteredData = filteredData.filter(row => row.datetime === date);
+    }
+  
+    // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+  }
+  
+  // Attach an event to listen for the form button
+  d3.selectAll("#filter-btn").on("click", handleClick);
+  
+  // Build the table when the page loads
+  buildTable(tableData);
+  
